e50297c01870904d8ebc7c0f932479a40aacf399
Sultan <sitexpressorg@gmail.com>
2022-11-07 18:38:33 +0400

    gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import Technologies from "./Technologies";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
-     Hello, samurai! Let's go!
+        <Header/>
+        <Technologies/>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/Technologies.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Technologies = () => {
+    return (
+        <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JS</li>
+        </ul>
+    )
+}
+
+export default Technologies
